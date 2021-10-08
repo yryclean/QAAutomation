0f01207177c0f68c2947d12b66654ce1308ddb2a
Irena <irenasovko@mail.ru>
2021-10-08 20:14:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+    static String classVeriable;
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World!");
+        classVeriable = "Hi";
+
+
+        String name;
+        name = "Yury";
+        String anotherName = name;
+        name = "Bill";
+
+        System.out.println(anotherName);
+        System.out.println(name);
+
+        for(int i = 0; i < 10; ++i) {
+            int x = 50;
+        }
+        }
+
+
+
+    }
+}
